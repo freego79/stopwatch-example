d97e7595788333ebf1920a934c3f5aeeac69c731
Petr Frycek <petr.frycek@seznam.cz>
2025-03-10 12:46:11 +0100

    Initial commit

--- app/src/main/java/cz/freego/tutorial/stopwatchexample/ui/theme/Theme.kt
+++ app/src/main/java/cz/freego/tutorial/stopwatchexample/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package cz.freego.tutorial.stopwatchexample.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
